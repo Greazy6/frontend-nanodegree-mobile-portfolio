567e000719ceb13935bc7a4e389187e2f8562cf3
Greazy6 <grclkrc@hotmail.com>
2015-03-24 15:57:32 -0400

    change back

--- views/js/main.js
+++ views/js/main.js
@@ -513,7 +513,7 @@ function updatePositions() {
     console.log("phase = " + phase);
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
-}
+  
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
   // Super easy to create custom metrics.
