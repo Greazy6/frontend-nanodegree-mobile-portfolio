303ef256ce7f7d57a88e428bb0694cae8521169e
Greazy6 <grclkrc@hotmail.com>
2015-03-24 10:44:44 -0400

    rest of test

--- views/js/main.js
+++ views/js/main.js
@@ -505,6 +505,10 @@ function updatePositions() {
   var items = document.querySelectorAll('.mover');
   console.log("======================= New call to updatePositions =======================");
   for (var i = 0; i < items.length; i++) {
+    console.log("----");
+    console.log("i = " + i);
+    console.log("i % 5 = " + (i % 5));
+    console.log("document.body.scrollTop = " + document.body.scrollTop);
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
